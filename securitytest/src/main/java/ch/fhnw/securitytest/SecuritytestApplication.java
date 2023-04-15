77541ed0aee1814fada41541437aad775abff34d
charutapande <32811202+charutapande@users.noreply.github.com>
2023-04-15 08:49:36 +0000

    Initial commit

---
+++ securitytest/src/main/java/ch/fhnw/securitytest/SecuritytestApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.securitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritytestApplication.class, args);
+	}
+
+}
